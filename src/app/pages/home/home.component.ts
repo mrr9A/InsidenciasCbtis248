33c98b402b14cd072b8a52b604e3b56f1250ca85
password <romero123mar123@gmail.com>
2024-10-20 17:35:11 -0600

    diseño del historial responsable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../../components/menu/menu.component";
 import { CardInfoAlumnoComponent } from "../../components/card-info-alumno/card-info-alumno.component";
+import { HistorialComponent } from "../historial/historial.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuComponent, CardInfoAlumnoComponent],
+  imports: [MenuComponent, CardInfoAlumnoComponent, HistorialComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

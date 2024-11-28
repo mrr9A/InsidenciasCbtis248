bcde3bc740930219d8782547e4615cbcb66f1289
password <romero123mar123@gmail.com>
2024-11-28 11:10:53 -0600

    SERVIDOR

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -11,16 +11,10 @@ export class WebSocketService {
 
   constructor(private notificationService: NotificationService) {
     /*     this.socket = io('http://localhost:3000'); */ // URL de tu servidor de WebSockets
-    this.socket = io('https://cbtis248zimatlan.onrender.com'); // URL de tu servidor de WebSockets
+    this.socket = io('http://3.21.170.124:3000'); // URL de tu servidor de WebSockets
   }
 
   // Conectar al WebSocket con el ID del responsable
-  /*   conectar(responsableId: string) {
-      this.responsableId = responsableId;
-      this.socket.emit('registrar-responsable', responsableId); // Evento para registrar al responsable
-      this.escucharNotificaciones(); // Escuchar las notificaciones específicas para este responsable
-    } */
-
   conectar(responsableId: string) {
     this.socket.emit('registrar-responsable', responsableId);
 
@@ -38,29 +32,6 @@ export class WebSocketService {
       }
     });
   }
-
-/*     conectar(responsableId: string) {
-      this.responsableId = responsableId;
-
-      // Emitir el evento para registrar al responsable
-      this.socket.emit('registrar-responsable', responsableId);
-
-      // Escuchar las notificaciones específicas para este responsable
-      this.socket.on(`notificacion-${responsableId}`, (mensaje: any) => {
-        // Enviar notificación al Service Worker
-        if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.ready.then((registration) => {
-            registration.showNotification(mensaje.titulo, {
-              body: mensaje.descripcion,
-              icon: 'assets/img/insidencia.png',
-              data: { url: `/cbtis248/avisos` }, // Usamos el ID del aviso para redirigir a la página del aviso
-            });
-          });
-        }
-      });
-
-    }
- */
 
   // Escuchar eventos de WebSocket para notificaciones
   escucharNotificaciones() {
